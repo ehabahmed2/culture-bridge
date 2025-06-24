d0f05d3d6337fcd2260d4959f022653735a4c000
Ehab <ihapbpc@gmail.com>
2025-06-25 01:10:16 +0200

    time to deploy

--- culture_mgmnt/settings.py
+++ culture_mgmnt/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1evs_3g6r!&$_*a0@rq8hupdia@lo0&sy-9%ny95k-8o75u*u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
